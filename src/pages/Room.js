60385fe664c4f9e2d70b8bd25088aa9f6fd7b783
Devign Experience <devignx@gmail.com>
2023-03-27 22:17:11 +0530

    ux updates : HP

--- src/pages/Room.js
+++ src/pages/Room.js
@@ -7,10 +7,24 @@ import { Link } from "react-router-dom";
 import logo from '../assets/spotlogo.svg'   
 import { BsSun, BsMoon } from 'react-icons/bs'
 import { AiOutlineHome } from 'react-icons/ai';
+import {RxCross2} from 'react-icons/rx'
+import {BiCheck} from 'react-icons/bi'
+import {GiSadCrab} from 'react-icons/gi'
 
 const Room = () => {
 
     const [people, setPeople] = useState(true)
+    const [connect, setConnect] = useState(false)   
+    const [buttonText, setButtonText] = useState("Connect")
+
+    const handleButtonText = () => {
+        if(connect==false){
+            setButtonText("Withdraw");
+        }
+        else{
+            setButtonText("Connect");
+        }
+    }
 
     const navigate = useNavigate();
     const { changeTheme, theme } = useStore((state) => ({changeTheme: state.changeTheme, theme: state.theme}))
@@ -77,44 +91,60 @@ const Room = () => {
             </div>
             <div className="w-full h-5/6 flex flex-col items-center justify-center">
                 
-            <div className="flex w-1/2 items-center justify-center ">
-                    <button onClick={()=> setPeople(true)} className={`text-lg p-2 w-full px-8 ${people? '': "opacity-50"} rounded-lg rounded-b-none rounded-tr-none pb-3 backhue`}>People</button>
-                    <button onClick={()=> setPeople(false)} className={`text-lg p-2 w-full px-8 ${people? 'opacity-50': ""} rounded-lg rounded-tl-none rounded-b-none pb-3 backhue`}>Requests <span className="bg-red-600 rounded-full p-1 px-2 text-xs">{requests.length}</span></button>
+            <div className="flex w-full md:w-1/2 items-center justify-center">
+                    <button onClick={()=> setPeople(true)} className={`text-lg p-2 w-full px-8 ${people? 'drop-shadow-xl z-[0]': "opacity-50"} transition-all duration-300 ease-in-out rounded-lg rounded-b-none rounded-tr-none pb-3 backhue`}>People ({peers.length})</button>
+                    <button onClick={()=> setPeople(false)} className={`text-lg p-2 w-full px-8 ${people? 'opacity-50': "drop-shadow-xl z-[0]"} transition-all duration-300 ease-in-out rounded-lg rounded-tl-none rounded-b-none pb-3 backhue`}>Requests <span className={`${requests.length==0 ? "bg-none": "bg-red-600 text-xs"} rounded-full text-white p-[3px] px-[7px]`}>{requests.length > 0 && `${requests.length}`}</span></button>
                 </div>
-                <div className="w-full md:w-1/2 h-5/6 backhue rounded-lg rounded-t-none p-4 overflow-scroll overflow-x-hidden">
+                <div className="w-full z-[4] md:w-1/2 h-5/6 backhue rounded-lg rounded-t-none transition-all duration-300 ease-in-out p-4 overflow-scroll overflow-x-hidden">
                     {
+                    peers.length == 0 
+                    && 
+                        <div className="w-full h-5/6 flex flex-col text-center justify-center items-center rale uppercase">
+                            <GiSadCrab className="opacity-20" size="8rem" />
+                            <p className="opacity-20 text-xl">You're the only on here</p>
+                            <p className="text-[0.7rem] opacity-60 tracking-widest mt-2">Try Inviting your friends inside your Network</p>
+                        </div>
+                        
+                    }
+                    {   
+                        
                         people?
                             peers.map((peer, index)=> <div key={index} className="w-full mb-4">
-                                <div className=" m-auto px-6 md:px-12 p-6 bg-blue-500/10 font-semibold rounded-lg mb-2 flex items-center justify-between w-full gap-4">
+                                <div className="anim m-auto px-6 md:px-12 p-6 bg-blue-500/10 font-semibold rounded-lg mb-2 flex items-center justify-between w-full gap-4">
                                     <div className="flex gap-3 items-center ">
                                         <BsPerson className="w-8 h-8 p-2 bg-white border-[1.5px] border-blue-500/50 rounded-full" color="#000" size={"30px"}/> 
                                         <h1>{peer.name}</h1>
                                     </div>
-                                    <div onClick={()=> sendRequest(peer)} className=" text-white cursor-pointer p-3 px-5 bg-blue-500 rounded-lg flex items-center justify-center">
-                                        <h1>Connect</h1>
-                                    </div>
+                                    <button onClick={()=> { sendRequest(peer); setConnect(!connect); handleButtonText(); }} className={`${connect ? "bg-gray-500" : ""} font-thin text-white cursor-pointer p-2 px-5 bg-blue-500 rounded-lg flex items-center justify-center`}>
+                                        {buttonText}
+                                    </button>
                                 </div>
-                                
+
                             </div>)
                         :
                             requests.map((peer, index)=> <div key={index} className="w-full mb-4">
-                                <div className=" m-auto px-6 md:px-12 p-6 bg-blue-500/10 font-semibold rounded-lg mb-2 flex items-center justify-between w-full gap-4">
+                                <div className="anim m-auto px-6 md:px-8 p-6 bg-blue-500/10 font-semibold rounded-lg mb-2 flex items-center justify-between w-full gap-4">
                                     <div className="flex gap-3 items-center ">
                                         <BsPerson className="w-8 h-8 p-2 bg-white border-[1.5px] border-blue-500/50 rounded-full" color="#000" size={"30px"}/> 
                                         <h1>{peer.name}</h1>
                                     </div>
-                                    <div className=" text-white cursor-pointer p-3 px-5 bg-blue-500 rounded-lg flex items-center justify-center">
-                                        <h1>Connect</h1>
+                                    <div className="flex gap-7">
+                                        <div className=" text-white font-thin cursor-pointer p-2 px-5 bg-blue-500 rounded-lg flex gap-2 items-center justify-center">
+                                            <h1 className="pclg">Accept</h1><BiCheck/>
+                                        </div>
+                                        <div className=" text-white font-thin text-sm cursor-pointer gap-2 p-2 px-5 bg-red-500 rounded-lg flex items-center justify-center">
+                                            <button className="pclg">Deny</button><RxCross2/>
+                                        </div>
                                     </div>
                                 </div>
                             </div>)
                     }
                 </div>
             </div>
-            <div className=" absolute backd z-[9999] p-2 rounded centerh bottom-4 flex justify-start gap-4">
+            <abbr title="Your Public IP Adress" className=" absolute backd z-[9999] p-2 rounded centerh bottom-4 flex justify-start gap-4">
                     <h1 className="text-sm uppercase">{name}</h1>
                     <h1 className="text-sm text-blue-500">{ip}</h1>
-            </div>
+            </abbr>
         </div>
     )
 }
