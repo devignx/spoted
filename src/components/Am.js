895539554ca90c90a1b49b33fa6ad15f8c342e5a
Devign Experience <devignx@gmail.com>
2023-03-29 10:09:11 +0530

    Update Am.js

--- src/components/Am.js
+++ src/components/Am.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Away = ({ message, sender }) => {
     return(
-        <div className="bg-white/10 text-lg shadow-md p-3 w-fit my-2 rounded-full max-w-1/2 rounded-bl-none px-4">
-            <h1 className="text-xs">{sender}</h1>
+        <div className="bg-white/10 text-lg shadow-md p-3 w-fit my-2 rounded-full max-w-1/2 rounded-bl-none max-x-10/12 px-4 md:px-6 ">
+            <h1 className="text-xs font-thin opacity-60">{sender}</h1>
             <h1>{message}</h1>
         </div>
     )
