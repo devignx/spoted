ca71cb1626bd41d6b54bc5f4835a49485f5be501
Devign Experience <devignx@gmail.com>
2023-05-18 23:12:33 +0530

    p2p chat revamp, minor ui tweeks

--- src/components/Chatpop.js
+++ src/components/Chatpop.js
@@ -3,12 +3,12 @@ import React, { useEffect } from "react";
 const ChatPop = ({ message, state }) => {
 
     useEffect(()=> {
-        setTimeout(()=> state(false), 5000)
+        setTimeout(()=> state(false), 15000)
     }, [])
 
     return(
-        <div className="absolute right-10 bottom-24 w-1/4 h-fit p-2 bg-gray-500 rounded-lg">
-            <h1>{message}</h1>
+        <div className="bg-blue-500 text-white shadow-md text-lg p-3 my-2 w-full rounded-[1rem] px-4">
+            <h1 className="strict-wrap break-words">{message}</h1>
         </div>
     )
 }
--- src/components/Mode.js
+++ src/components/Mode.js
@@ -3,6 +3,7 @@ import {BsCameraVideoFill,BsFillMicFill, BsChatSquareTextFill} from 'react-icons
 import { Link, useNavigate } from 'react-router-dom';
 import { AiOutlineSend } from 'react-icons/ai';
 import ChatPop from './Chatpop';
+import {IoMdNotifications} from 'react-icons/io'
 
 const Mode = ({ dc }) => {
 
@@ -62,30 +63,49 @@ const Mode = ({ dc }) => {
     }
 
     return (
-        <div id='popup' className='fixed anim top-0 text-black w-full z-[99999]'> 
-            <div className='relative flex w-full h-screen backd anim items-center justify-center'>
-                <p className=' font-semibold absolute text-center text-sm top-12'>Welcome to <br/><span className='text-blue-500 text-lg'>SPOT P2P Chat</span></p>
-                <div className='px-8 py-12 md:p-12 anim text-center w-full md:w-1/2 rounded-lg backhue relative drop-shadow-xl'>
-                    <p className='font-semibold '>Choose your Mode</p>
-                    <div className='flex flex-wrap gap-8 mt-8'>
-                        <Link to = '/private' className='min-w-[7rem] md:min-w-[10rem] grow  basis-1  flex flex-col gap-3 justify-center items-center min-h-[7rem] md:min-h-[10rem] text-xs rounded-2xl backd m-auto uppercase font-semibold'><BsFillMicFill size='25px'/><p className='opacity-50'> Audio chat</p></Link>
-                        <div onClick={handleVedeo} className='min-w-[7rem] md:min-w-[10rem] basis-1 grow flex flex-col gap-3 justify-center items-center min-h-[7rem] md:min-h-[10rem] text-xs rounded-2xl backd m-auto uppercase font-semibold'><BsCameraVideoFill size='25px'/><p className='opacity-50'> Video Call</p></div>
-                        <div onClick={handleFile} className='min-w-[7rem] md:min-w-[10rem] basis-1 grow w-full flex flex-col gap-3 justify-center items-center min-h-[7rem] md:min-h-[10rem] text-xs rounded-2xl backd m-auto uppercase font-semibold'><BsChatSquareTextFill size='25px'/><p className='opacity-50'> Texting</p></div>
-                        <button className='w-10 h-10 backd absolute border-2 border-red-600 centerh rounded-full font-semibold -bottom-5'>X</button>
+        <div id='popup' className='anim text-black z-[99999]'> 
+            <div className='relative backd '>
+                <p className=' font-semibold text-center  text-sm mt-6 md:mt-16'>Welcome to <br/><span className='text-blue-500 text-lg'>SPOT P2P Chat</span></p>
+                <div className='p-12 pb-16 anim mx-auto mt-4 md:mt-12 mb-36 text-center w-full md:w-1/2 rounded-lg backhue relative drop-shadow-xl'>
+                    <div className='rale relative mx-auto w-fit'>
+                        { ismsg ? 
+                        
+                        <div className='flex flex-col justify-center items-center gap-3'>
+                            <div className='relative'>
+                                <IoMdNotifications size='25px' />
+                                <div className='w-2 h-2 rounded-full bg-red-500 absolute top-0 -right-0' ><br/></div>
+                            </div>
+                            Yay New message from your peer
+                        </div>
+                        : 
+                        'Choose your Mode'}
+                    </div>
+                    <div className={`${ismsg ? 'hidden' : ''} anim flex flex-wrap gap-8 mt-8`}>
+                        <Link to = '/private' className='min-w-[7rem] grow  basis-1  flex flex-col gap-3 justify-center items-center h-[7rem] text-xs rounded-2xl backd m-auto uppercase font-semibold'><BsFillMicFill size='25px'/><p className='opacity-50'> Audio chat</p></Link>
+                        <div onClick={handleVedeo} className='min-w-[7rem] basis-1 grow flex flex-col gap-3 justify-center items-center h-[7rem] text-xs rounded-2xl backd m-auto uppercase font-semibold'><BsCameraVideoFill size='25px'/><p className='opacity-50'> Video Call</p></div>
+                        <div onClick={handleFile} className='min-w-[7rem] basis-1 grow w-full flex flex-col gap-3 justify-center items-center h-[7rem] text-xs rounded-2xl backd m-auto uppercase font-semibold'><BsChatSquareTextFill size='25px'/><p className='opacity-50'> Texting</p></div>
+                        <button onClick={close} className='p-2 px-4 md:px-6 shrink-0 whitespace-nowrap absolute border-2 bg-red-500/10 border-red-600 backdrop-blur-lg topppp centerh rounded-full font-semibold -bottom-5'>Close connection &nbsp; X</button>
+                    </div>
+                    <br/>
+                    <div className={`${ismsg ? 'p-0' : 'p-3'} md:mt-6 anim relative w-full md:min-w-1/2  mx-auto backd rounded-full`}>
+                    {
+                        ismsg ? 
+                        <ChatPop  message={msg} state={setImsg}/>
+                        :
+                        <div className='flex gap-5 justify-center'>
+                            <p className='opacity-40 rale'>send / wait for a msg</p>
+                            <div className="lds-ripple block scale-50"></div>
+                        </div>
+                        
+                    }
                     </div>
                 </div>
-                {
-                    ismsg && <ChatPop message={msg} state={setImsg}/>
-                }
                 <div className="absolute bottom-0 w-full h-20">
                     <form onClick={handleMessage} className='w-full h-full flex items-center justify-center gap-4'>
-                        <input value={message} onChange={(event) => setMessage(event.target.value)} type='text' className='w-5/6 h-3/4 outline-0 bg-gray-500 rounded-lg p-4' placeholder='Type message'/>
-                        <button type='submit' className='p-4 rounded-full bg-blue-500'><AiOutlineSend/></button>
+                        <input value={message} onChange={(event) => setMessage(event.target.value)} type='text' className='outline-none bg-black/[.05] drop-shadow-xl m-auto text-lg fixed w-[94%] lg:w-[48%] mb-5 border-solid border-[1px] border-white/30 backdrop-blur-xl rounded-full bottom-0 px-5 py-3 md:py-4 ' placeholder='Type message'/>
+                        <button type='submit' className='fixed bottom-[2.5rem] lg:bottom-[2.7rem] right-[10%] lg:right-[28%] scale-[150%]'><AiOutlineSend/></button>
                     </form>
                 </div>
-                <button onClick={close} className='absolute bottom-20 right-10'>
-                    Close connection
-                </button>
             </div>
         </div>
     );
