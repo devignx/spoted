d8b2ae5b761d22b5e00476f185e63c85d654733f
Yasar195 <imyasar07@gmail.com>
2023-05-31 11:49:35 +0530

    changes

--- src/pages/File.js
+++ src/pages/File.js
@@ -7,65 +7,92 @@ const File = () => {
 
     const [files, setFiles] = useState([])
     const [file, setFile] = useState()
+    let is_file = true
+    let storedArrayBuffer = [];
 
-    function fileToBase64(file) {
-        return new Promise((resolve, reject) => {
-          const reader = new FileReader();
-          reader.onloadend = () => {
-            const base64String = reader.result.split(',')[1];
-            resolve(base64String);
-          };
-          reader.onerror = reject;
-          reader.readAsDataURL(file);
-        });
+    function mergeArrayBuffers() {
+        const totalLength = storedArrayBuffer.reduce((acc, arrayBuffer) => acc + arrayBuffer.byteLength, 0);
+        const mergedArrayBuffer = new Uint8Array(totalLength);
+        let offset = 0;
+      
+        for (const arrayBuffer of storedArrayBuffer) {
+          const chunk = new Uint8Array(arrayBuffer);
+          mergedArrayBuffer.set(chunk, offset);
+          offset += chunk.byteLength;
+        }
+      
+        return mergedArrayBuffer.buffer;
+      }
+
+    function convertArrayBufferToBlob(mimeType, merged) {
+        const blob = new Blob([merged], { type: mimeType });
+        return blob;
     }
 
-    function base64ToFile(base64String, fileName) {
-        return new Promise((resolve, reject) => {
-          fetch(`data:application/octet-stream;base64,${base64String}`)
-            .then(response => response.blob())
-            .then(data => {
-              const file = new File([data], fileName, { type: data.type });
-              resolve(file);
-            })
-            .catch(error => {
-              reject(error);
-            });
-        });
+    function saveBlob(blob, fileName) {
+        const blobUrl = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = blobUrl;
+        link.download = fileName;
+        link.click();
+    }
+
+    function sendFile(file) {
+        const chunkSize = 16384;
+        // Read the file as an ArrayBuffer
+        const reader = new FileReader();
+        reader.onload = (event) => {
+          const fileData = event.target.result;
+          let offset = 0;
+          // Send the file data through the data channel
+          function sendChunk() {
+            const chunk = fileData.slice(offset, offset + chunkSize);
+            if (chunk.byteLength === 0) {
+              console.log('File sent successfully');
+              dc.send('EOF')
+              return;
+            }
+            dc.send(chunk);
+            offset += chunkSize;
+            setTimeout(sendChunk, 100); // Delay between sending chunks (optional)
+          }
+          sendChunk()
+        };
+        reader.readAsArrayBuffer(file);
       }
     
-    const changeHandler = async (file) => {
-        console.log(file)
-        fileToBase64(file[0])
-        .then((data) => {
-            const payload = {
-                type: 'meta',
-                name: file[0].name,
-                size: file[0].size,
-            }
-            dc.send(JSON.stringify(payload))
-        })
+    const changeHandler = (file) => {
+        console.log(file.type)
+        dc.send(JSON.stringify({
+            type: 'meta',
+            name: file.name,
+            size: file.size,
+            type: file.type
+        }))
+        if (file) {
+            sendFile(file);
+        }
     }
 
     dc.onmessage = e => {
-        const data = JSON.parse(e.data)
-        console.log(e.data)
-        if(data){
-            if(data.type === "meta"){
-                const temp = files
-                temp.push(data)
-                setFiles([...temp])
-            }
-
-            if(data.type === 'EOF'){
-                console.log('end')
-            }
+        if(e.data === 'EOF'){
+            console.log('file recieved successfully')
+            let data = storedArrayBuffer.splice(0,1)
+            data = JSON.parse(data[0])
+            console.log(data)
+            const merged = mergeArrayBuffers()
+            const blob = convertArrayBufferToBlob(data.type, merged)
+            saveBlob(blob, data.name)
+        }
+        else{
+            console.log(e.data)
+            storedArrayBuffer.push(e.data)
         }
     };
 
     return (
         <div className="w-full h-screen">
-            <input type="file" onChange={(e) => changeHandler(e.target.files)}/>
+            <input type="file" onChange={(e) => changeHandler(e.target.files[0])}/>
             <div>
                 {
                     files.map((file, index) => <div key={index} className="w-full h-20 flex items-center justify-start gap-8">
